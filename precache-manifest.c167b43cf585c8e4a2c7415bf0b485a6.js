b26f43002cc3b9d689d66eb2c4c7b85383c1595b
kolebjak <jakub@kolebaba.cz>
2020-07-01 13:05:50 +0000

    Updates

---
+++ precache-manifest.c167b43cf585c8e4a2c7415bf0b485a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dffd55fab37f904b8e5dd80f4445d204",
+    "url": "/mosaic/index.html"
+  },
+  {
+    "revision": "6e21b8ff24a893c0715a",
+    "url": "/mosaic/static/css/2.c27a359e.chunk.css"
+  },
+  {
+    "revision": "30ec563ea8dccd3aa236",
+    "url": "/mosaic/static/css/main.4d38596f.chunk.css"
+  },
+  {
+    "revision": "6e21b8ff24a893c0715a",
+    "url": "/mosaic/static/js/2.50b07e2a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/mosaic/static/js/2.50b07e2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30ec563ea8dccd3aa236",
+    "url": "/mosaic/static/js/main.861ae6da.chunk.js"
+  },
+  {
+    "revision": "6b176efd1ba1940b02a2",
+    "url": "/mosaic/static/js/runtime-main.80545166.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mosaic/precache-manifest.9502a9e8c563dc4b222b12d947a05ad3.js"
+  "/mosaic/precache-manifest.c167b43cf585c8e4a2c7415bf0b485a6.js"
 );
 
 self.addEventListener('message', (event) => {
